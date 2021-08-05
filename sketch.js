f7fc743d0080aa4cc9249dcf27503b39a20cc442
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2021-08-05 20:00:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var FoodGroup, obstacleGroup
 var  over,over1
 function preload()
 {
-  monkey_running =                            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")  
+  monkey_running =loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")  
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png"); 
 
@@ -16,12 +16,12 @@ function preload()
 
 function setup()
 {
-  createCanvas(1040,700)
+  createCanvas(displayWidth,displayHeight)
   monkey=createSprite(100,400)
   monkey.addAnimation("moving",monkey_running)
   monkey.scale=0.25
   
-  ground=createSprite(500,700,5000,40)
+  ground=createSprite(di,700,5000,40)
   ground.velocityX=-5
 
   FoodGroup=new Group()
@@ -100,8 +100,8 @@ function bananaMaker()
     
     banana = createSprite(100,400,10,40);
     banana.addImage("bun",bananaImage)
-    banana.x=Math.round(random(100,700))
-    banana.y=Math.round(random(400,700))
+    banana.x=Math.round(random(displayWidth,displayWidth))
+    banana.y=Math.round(random(displayHeight,displayHeight))
     banana.velocityY=3
     banana.lifetime=100
     banana.scale=0.25
