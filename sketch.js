5660c3a89d381513a65c548da26c2e3851088da3
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2021-08-10 14:44:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var FoodGroup, obstacleGroup
 var  over,over1
 function preload()
 {
+  bg1=loadImage("bg.jpg")
   monkey_running =loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")  
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png"); 
@@ -31,15 +32,16 @@ function setup()
 
 function draw()
 {
+
+background(bg1);
   var x =200;
     var y;
   camera.position.x = monkey.x
-  camera.position.y = monkey.y
+  camera.position.y = displayHeight/2
   if(ground.x<0)
     {
       ground.x=ground.width/2
-    }
-  background("green") 
+
   if (GAMESTATE===1)
   {  
   
@@ -95,6 +97,7 @@ function draw()
   
   text("Survival  " +  survival,600,50)
   drawSprites(); 
+}
 }
 
 function bananaMaker()
